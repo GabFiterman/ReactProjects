ce67234d6545a3b293a023cc07d93f2e1c3e6988
Fiterman <gfiterman96@gmail.com>
2022-04-26 12:23:24 -0300

    File Structure
    Basically, the organization and structure of files was created. The project's basic CSS has also been established.

---
+++ gpt3/src/components/index.js
@@ -0,0 +1,9 @@
+//This file is used to export all 'jsx' components and be able to
+//import them at once in a simpler way
+//(just importing only this index.js file)
+
+export { default as Article } from './article/Article';
+export { default as Brand } from './brand/Brand';
+export { default as Cta } from './cta/Cta';
+export { default as Feature } from './feature/Feature';
+export { default as Navbar } from './navbar/Navbar';
---
+++ gpt3/src/containers/index.js
@@ -0,0 +1,10 @@
+//This file is used to export all 'jsx' components and be able to
+//import them at once in a simpler way
+//(just importing only this index.js file)
+
+export { default as Blog } from './blog/Blog';
+export { default as Features } from './features/Features';
+export { default as Footer } from './footer/Footer';
+export { default as Header } from './header/Header';
+export { default as Possibility } from './possibility/Possibility';
+export { default as WhatGPT3 } from './whatGPT3/WhatGPT3';
